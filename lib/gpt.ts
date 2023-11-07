5fc5a59fe63be72758464017cebcfdc8da1df80e
aboogamer <yaseenaboobaker1@gmail.com>
2023-11-07 22:26:00 +0200

    lets go

--- lib/gpt.ts
+++ lib/gpt.ts
@@ -3,6 +3,8 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+console.log(process.env.OPENAI_API_KEY)
 const openai = new OpenAIApi(configuration);
 
 interface OutputFormat {
