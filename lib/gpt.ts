6509e417c9e31c1f805a3133bcf07f489f07d54a
aboogamer <yaseenaboobaker1@gmail.com>
2023-11-07 22:44:29 +0200

    last try

--- lib/gpt.ts
+++ lib/gpt.ts
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 
 const apiKey = process.env.OPENAI_API_KEY
-
+console.log(apiKey)
 const configuration = new Configuration({
   apiKey: apiKey,
 });
