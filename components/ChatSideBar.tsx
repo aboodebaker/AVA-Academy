0ef79035a8e32c21183e96072e984452967c9ba8
Yaseen Aboobaker <79657104+aboodebaker@users.noreply.github.com>
2023-11-08 11:35:39 +0200

    Update ChatSideBar.tsx

--- components/ChatSideBar.tsx
+++ components/ChatSideBar.tsx
@@ -37,7 +37,7 @@ const ChatSideBar = ({ chats, chatId, isPro }: Props) => {
         {chats.map((chat) => (
           <Link key={chat.id} href={`/chat/${chat.id}`}>
             <div
-              className={cn("rounded-lg p-3 text-slate-300 flex items-center", {
+              className={cn("rounded-lg p-3 text-white flex items-center", {
                 "bg-blue-600 text-white": chat.id === chatId,
                 "hover:text-white": chat.id !== chatId,
               })}
