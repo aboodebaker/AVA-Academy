dc73591306d21cdf7092bf88ad3890d5f6fac29d
Yaseen Aboobaker <79657104+aboodebaker@users.noreply.github.com>
2024-03-11 22:56:49 +0200

    Update carasoul.jsx

--- components/carousel/carasoul.jsx
+++ components/carousel/carasoul.jsx
@@ -55,7 +55,7 @@ const CarouselItem = ({ notes }) => {
 
 
   useEffect(() => {
-    const divWidth = screenWidth > 800 ? screenWidth - number -20 : screenWidth 
+    const divWidth = screenWidth > 800 ? screenWidth - number -20 : screenWidth - 20
     document.querySelector('.scrolling-wrapper').style.width = `${divWidth}px`;
   }, [number, screenWidth]);
 
