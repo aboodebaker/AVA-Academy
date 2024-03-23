0b2742557a3064986f9de1bd641986599ff93345
aboogamer <yaseenaboobaker1@gmail.com>
2024-03-23 15:39:36 +0200

    fixed

--- app/api/classes/route.ts
+++ app/api/classes/route.ts
@@ -11,13 +11,19 @@ const prisma = await new PrismaClient()
   const { id } = await req.json();
 
   const subjects = await prisma.subject.findMany({
-    where: {
-      userId: id
-    }
-  })
+  where: {
+    userId: id
+  }
+});
 
-  const classes = {subjects}
+const plainSubjects = subjects.map(subject => {
+  return {
+    id: subject.id,
+    name: subject.name,
+    picture: subject.image,
+  };
+});
 
-  return new NextResponse(classes, {status: 200})
+  return new NextResponse(plainSubjects, {status: 200})
 
 }
